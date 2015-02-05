1a851956c4f1c5e12c9a79eb377b9afd40960379
Sam Clements <sam@borntyping.co.uk>
2015-02-05 16:22:42 +0000

    Return IoResult when reading and writing pidfiles

--- src/pidfile.rs
+++ src/pidfile.rs
@@ -1,15 +1,26 @@
 //! Contains functions to read and write pidfiles
 
 use std::old_io::File;
+use std::old_io::IoError;
+use std::old_io::IoErrorKind;
+use std::old_io::IoResult;
 use std::old_io::Writer;
 use std::str::FromStr;
 
-pub fn write_pidfile(path: &Path) {
-    write!(&mut File::create(path).unwrap(), "{}", super::getpid()).unwrap();
+pub fn write_pidfile(path: &Path) -> IoResult<()> {
+    return write!(&mut File::create(path).unwrap(), "{}", super::getpid());
 }
 
-pub fn read_pidfile(path: &Path) -> super::PID {
-    let mut file = File::open(path).unwrap();
-    let contents = file.read_to_string().unwrap();
-    return FromStr::from_str(contents.as_slice()).unwrap();
+pub fn read_pidfile(path: &Path) -> IoResult<super::PID> {
+    let mut file = try!(File::open(path));
+    let contents = try!(file.read_to_string());
+
+    return match FromStr::from_str(contents.as_slice()) {
+        Ok(pid) => Ok(pid),
+        Err(_)  => Err(IoError {
+            kind: IoErrorKind::InvalidInput,
+            desc: "Could not parse pidfile as PID",
+            detail: Some(contents)
+        })
+    };
 }
