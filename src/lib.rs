44024aa62ec9c7154b2ba9dd158ee8666a447e44
Sam Clements <sam@borntyping.co.uk>
2015-02-06 14:05:22 +0000

    Mark PID, getpid and getppid as stable

--- src/lib.rs
+++ src/lib.rs
@@ -15,14 +15,17 @@ pub mod system;
 /// Type for process identifiers
 ///
 /// This should expand to `i32` (signed 32 bit integer).
+#[stable]
 pub type PID = libc::types::os::arch::posix88::pid_t;
 
 /// Return the PID of the calling process
+#[stable]
 pub fn getpid() -> PID {
     unsafe { libc::funcs::posix88::unistd::getpid() }
 }
 
 /// Return the PID of the parent process
+#[stable]
 pub fn getppid() -> PID {
     unsafe { libc::funcs::posix88::unistd::getppid() }
 }
