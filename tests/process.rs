5bfbb59eeba595d7a39bb3e5e2049814ec877e37
Sam Clements <sam@borntyping.co.uk>
2015-03-06 20:51:04 +0000

    Add tests for psutil::process::Process

---
+++ tests/process.rs
@@ -0,0 +1,34 @@
+extern crate psutil;
+
+use psutil::getpid;
+use psutil::process::Process;
+
+fn get_process() -> Process {
+    Process::new(getpid()).unwrap()
+}
+
+#[test]
+fn process() {
+    assert!(Process::new(getpid()).is_ok());
+}
+
+#[test]
+fn process_alive() {
+    assert!(get_process().is_alive());
+}
+
+#[test]
+fn process_cmdline() {
+    assert!(get_process().cmdline().is_ok());
+}
+
+#[test]
+fn process_extended_name() {
+    assert!(get_process().extended_name().len() > 0);
+}
+
+
+#[test]
+fn process_memory() {
+    assert!(get_process().memory().is_ok());
+}
