c3ca8f1a23c83258430fe9cef4381a976d96b2ef
Rob Day <rkd@rkd.me.uk>
2018-02-02 22:01:54 +0000

    Use fake version of /proc filesystem in tests

--- src/system.rs
+++ src/system.rs
@@ -1,15 +1,14 @@
 //! Read information about the operating system from `/proc`.
 
 use std::str::FromStr;
-use std::path::Path;
 
-use utils::read_file;
+use utils::read_proc_file;
 
 /// Returns the system uptime in seconds.
 ///
 /// `/proc/uptime` contains the system uptime and idle time.
 pub fn uptime() -> isize {
-    let data = read_file(Path::new("/proc/uptime")).unwrap();
+    let data = read_proc_file("uptime").unwrap();
     let numbers: Vec<&str> = data.split(' ').collect();
     let uptime: Vec<&str> = numbers[0].split('.').collect();
     FromStr::from_str(uptime[0]).unwrap()
