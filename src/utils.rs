68ce573bd71aab5244013df4a694d44ff7a20943
Rob Day <rkd@rkd.me.uk>
2018-02-06 21:58:07 +0000

    Revert "Use fake version of /proc filesystem in tests"
    This reverts commit c3ca8f1a23c83258430fe9cef4381a976d96b2ef.

--- src/utils.rs
+++ src/utils.rs
@@ -3,19 +3,10 @@
 use std::fs::File;
 use std::io::{Read, Result};
 use std::path::Path;
-use std::env;
 
 pub fn read_file(path: &Path) -> Result<String> {
     let mut buffer = String::new();
     let mut file = try!(File::open(path));
     try!(file.read_to_string(&mut buffer));
     Ok(buffer)
-}
-
-
-pub fn read_proc_file(subpath: &str) -> Result<String> {
-    let default_procdir = "/proc".to_string();
-    let procdir = env::var("PSUTIL_PROCDIR").unwrap_or(default_procdir);
-    let path = Path::new(&procdir).join(subpath);
-    read_file(&path)
 }
--- tests/system.rs
+++ tests/system.rs
@@ -1,12 +1,6 @@
 extern crate psutil;
 
-use std::path::PathBuf;
-use std::env;
-
 #[test]
 fn uptime() {
-    let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-    d.push("resources/test/fakeproc");
-    env::set_var("PSUTIL_PROCDIR", d.to_str().unwrap());
-    assert!(psutil::system::uptime() == 16);
+    assert!(psutil::system::uptime() > 0);
 }
