22110243e032a356226ed1f438f32409f9e75dd9
Ethan Smith <ethan@ethanhs.me>
2018-08-08 22:45:04 -0700

    Don't panic, map error instead.

--- src/utils.rs
+++ src/utils.rs
@@ -9,7 +9,11 @@ pub fn read_file(path: &Path) -> Result<String> {
     let mut buffer = Vec::with_capacity(metadata.len() as usize);
     let mut file = try!(fs::File::open(path));
     try!(file.read_exact(&mut buffer));
-    Ok(String::from_utf8(buffer).unwrap())
+    let metadata = try!(fs::metadata(path));
+    let mut buffer = Vec::with_capacity(metadata.len() as usize);
+    let mut file = try!(fs::File::open(path));
+    try!(file.read_exact(&mut buffer));
+    String::from_utf8(buffer).map_err(|err| Error::new(ErrorKind::Other, err.to_string()))
 }
 
 macro_rules! try_parse {
