cdb4039c1379e9c32b331687eb311390e034f52f
Caleb Bassi <calebjbassi@gmail.com>
2019-04-08 16:44:59 -0700

    Readd system.rs for backwards compat

--- src/linux/mod.rs
+++ src/linux/mod.rs
@@ -5,3 +5,4 @@ pub mod memory;
 pub mod network;
 pub mod process;
 pub mod sensors;
+pub mod system;
---
+++ src/linux/system.rs
@@ -0,0 +1,4 @@
+pub use crate::linux::cpu::*;
+pub use crate::linux::host::*;
+pub use crate::linux::memory::*;
+pub use crate::utils::not_found;
