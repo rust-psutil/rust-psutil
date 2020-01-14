1eadc203e24e7e4d3227ceb0c49905259b0d553b
Caleb Bassi <calebjbassi@gmail.com>
2020-01-14 15:06:05 -0800

    Move disk usage from linux to unix

--- src/disk/sys/linux/mod.rs
+++ src/disk/sys/linux/mod.rs
@@ -1,7 +1,5 @@
-mod partitions;
 mod disk_io_counters;
-mod disk_usage;
+mod partitions;
 
-pub use partitions::*;
 pub use disk_io_counters::*;
-pub use disk_usage::*;
+pub use partitions::*;
--- src/disk/sys/mod.rs
+++ src/disk/sys/mod.rs
@@ -3,3 +3,9 @@ mod linux;
 
 #[cfg(target_os = "linux")]
 pub use linux::*;
+
+#[cfg(target_family = "unix")]
+mod unix;
+
+#[cfg(target_family = "unix")]
+pub use unix::*;
