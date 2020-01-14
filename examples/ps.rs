228a5956e6093640ddaaaf0347876eda1fdd51aa
Caleb Bassi <calebjbassi@gmail.com>
2020-01-14 12:37:08 -0800

    Update ps example

--- examples/ps.rs
+++ examples/ps.rs
@@ -1,23 +1,28 @@
+use std::thread;
+use std::time::Duration;
+
 use psutil::process::processes;
 
 // TODO: update to actually match the output of `ps aux`
 
 fn main() {
-    println!(
-        "{:>5} {:^5} {:>8} {:>8} {:.100}",
-        "PID", "STATE", "UTIME", "STIME", "CMD"
-    );
+    let processes = processes().unwrap();
 
-    for p in processes().unwrap() {
-        let p = p.unwrap();
-        let cpu_times = p.cpu_times().unwrap();
+    let block_time = Duration::from_millis(1000);
+    thread::sleep(block_time);
+
+    println!(
+        "{:>6} {:>4} {:>4} {:.100}",
+        "PID", "%CPU", "%MEM", "COMMAND"
+    );
+    for p in processes {
+        let mut p = p.unwrap();
 
         println!(
-            "{:>5} {:^5?} {:>8.2?} {:>8.2?} {:.100}",
+            "{:>6} {:>2.1} {:>2.1} {:.100}",
             p.pid(),
-            p.status().unwrap(),
-            cpu_times.user(),
-            cpu_times.system(),
+            p.cpu_percent().unwrap(),
+            p.memory_percent().unwrap(),
             p.cmdline()
                 .unwrap()
                 .unwrap_or_else(|| format!("[{}]", p.name().unwrap())),
