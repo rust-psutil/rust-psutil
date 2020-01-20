2604693f391a2e613549104e04a6466c160878ba
Caleb Bassi <calebjbassi@gmail.com>
2020-01-20 07:17:28 -0800

    Move cpu collectors around

--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -1,6 +1,10 @@
 mod cpu_count;
+mod cpu_percent;
+mod cpu_times_percent;
 pub mod os;
 mod sys;
 
 pub use cpu_count::*;
+pub use cpu_percent::*;
+pub use cpu_times_percent::*;
 pub use sys::*;
--- src/cpu/sys/mod.rs
+++ src/cpu/sys/mod.rs
@@ -3,9 +3,3 @@ mod linux;
 
 #[cfg(target_os = "linux")]
 pub use linux::*;
-
-mod cpu_percent;
-mod cpu_times_percent;
-
-pub use cpu_percent::*;
-pub use cpu_times_percent::*;
