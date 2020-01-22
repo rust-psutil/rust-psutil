f8237dc4951dcbf7b104297f7d7e42b6938c70f9
Caleb Bassi <calebjbassi@gmail.com>
2020-01-22 13:16:58 -0800

    [network][linux] refactor io counters

--- src/network/sys/linux/net_io_counters.rs
+++ src/network/sys/linux/net_io_counters.rs
@@ -1,17 +1,15 @@
 use std::collections::HashMap;
 use std::fs;
 use std::io;
+use std::str::FromStr;
 
 use crate::network::NetIoCounters;
 use crate::utils::invalid_data;
 
-pub(crate) fn net_io_counters_pernic() -> io::Result<HashMap<String, NetIoCounters>> {
-	let net_dev = fs::read_to_string("/proc/net/dev")?;
-	let net_lines: Vec<&str> = net_dev.lines().collect();
+impl FromStr for NetIoCounters {
+	type Err = std::io::Error;
 
-	let mut io_counters = HashMap::new();
-
-	for line in net_lines.iter().skip(2) {
+	fn from_str(line: &str) -> Result<Self, Self::Err> {
 		let fields: Vec<&str> = line.split_whitespace().collect();
 
 		if fields.len() < 17 {
@@ -20,24 +18,37 @@ pub(crate) fn net_io_counters_pernic() -> io::Result<HashMap<String, NetIoCounte
 			));
 		}
 
-		let mut net_name = String::from(fields[0]);
-		// remove the trailing colon
-		net_name.pop();
-
-		io_counters.insert(
-			net_name,
-			NetIoCounters {
-				bytes_sent: try_parse!(fields[9]),
-				bytes_recv: try_parse!(fields[1]),
-				packets_sent: try_parse!(fields[10]),
-				packets_recv: try_parse!(fields[2]),
-				err_in: try_parse!(fields[3]),
-				err_out: try_parse!(fields[11]),
-				drop_in: try_parse!(fields[4]),
-				drop_out: try_parse!(fields[12]),
-			},
-		);
+		Ok(NetIoCounters {
+			bytes_sent: try_parse!(fields[9]),
+			bytes_recv: try_parse!(fields[1]),
+			packets_sent: try_parse!(fields[10]),
+			packets_recv: try_parse!(fields[2]),
+			err_in: try_parse!(fields[3]),
+			err_out: try_parse!(fields[11]),
+			drop_in: try_parse!(fields[4]),
+			drop_out: try_parse!(fields[12]),
+		})
 	}
+}
 
-	Ok(io_counters)
+pub(crate) fn net_io_counters_pernic() -> io::Result<HashMap<String, NetIoCounters>> {
+	fs::read_to_string("/proc/net/dev")?
+		.lines()
+		.skip(2)
+		.map(|line| {
+			let fields: Vec<&str> = line.split_whitespace().collect();
+
+			if fields.len() < 17 {
+				return Err(invalid_data(
+					"'/proc/net/dev' does not have the right number of fields",
+				));
+			}
+
+			let mut net_name = String::from(fields[0]);
+			// remove the trailing colon
+			net_name.pop();
+
+			Ok((net_name, NetIoCounters::from_str(&line)?))
+		})
+		.collect()
 }
