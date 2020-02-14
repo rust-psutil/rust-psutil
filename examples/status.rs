a5c806ea40a8bc3b3e3c8f4b96978d14d44d87cc
Caleb Bassi <calebjbassi@gmail.com>
2020-02-14 08:27:25 -0800

    cleanup examples

--- examples/status.rs
+++
@@ -1,9 +0,0 @@
-use psutil::{host, process};
-
-fn main() {
-	println!("Process count: {}", process::pids().unwrap().len());
-	println!(
-		"System uptime: {} seconds",
-		host::uptime().unwrap().as_secs()
-	);
-}
