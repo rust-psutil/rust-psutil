9baa93b3ef3f770883bc443a63c6bc68e8e79174
Artur Kowalski <arturkow2000@gmail.com>
2020-07-16 23:22:26 +0200

    Format code

--- src/disk/os/linux.rs
+++ src/disk/os/linux.rs
@@ -4,40 +4,40 @@ use crate::disk::DiskIoCounters;
 use crate::Count;
 
 pub trait DiskIoCountersExt {
-    /// Time spent reading from disk.
-    fn read_time(&self) -> Duration;
+	/// Time spent reading from disk.
+	fn read_time(&self) -> Duration;
 
-    /// Time spent writing to disk.
-    fn write_time(&self) -> Duration;
+	/// Time spent writing to disk.
+	fn write_time(&self) -> Duration;
 
-    /// Time spent doing actual I/Os.
-    fn busy_time(&self) -> Duration;
+	/// Time spent doing actual I/Os.
+	fn busy_time(&self) -> Duration;
 
-    /// Number of merged reads.
-    fn read_merged_count(&self) -> Count;
+	/// Number of merged reads.
+	fn read_merged_count(&self) -> Count;
 
-    /// Number of merged writes.
-    fn write_merged_count(&self) -> Count;
+	/// Number of merged writes.
+	fn write_merged_count(&self) -> Count;
 }
 
 impl DiskIoCountersExt for DiskIoCounters {
-    fn read_time(&self) -> Duration {
-        self.read_time
-    }
+	fn read_time(&self) -> Duration {
+		self.read_time
+	}
 
-    fn write_time(&self) -> Duration {
-        self.write_time
-    }
+	fn write_time(&self) -> Duration {
+		self.write_time
+	}
 
-    fn busy_time(&self) -> Duration {
-        self.busy_time
-    }
+	fn busy_time(&self) -> Duration {
+		self.busy_time
+	}
 
-    fn read_merged_count(&self) -> Count {
-        self.read_merged_count
-    }
+	fn read_merged_count(&self) -> Count {
+		self.read_merged_count
+	}
 
-    fn write_merged_count(&self) -> Count {
-        self.write_merged_count
-    }
+	fn write_merged_count(&self) -> Count {
+		self.write_merged_count
+	}
 }
