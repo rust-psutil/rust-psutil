bbf450a175e1af10fc7b9f8a55ee1aaefb7c88d5
Rob Day <rkd@rkd.me.uk>
2022-12-04 23:07:54 +0000

    Suppress clippy lint that breaks MacOS portability

--- src/disk/sys/unix/disk_usage.rs
+++ src/disk/sys/unix/disk_usage.rs
@@ -34,22 +34,23 @@ impl DiskUsage {
 	}
 }
 
+// Disable the unnecessary_cast lint, as we need to do the u64 casts since on some platforms the
+// types are aliased to u32.
+// See https://github.com/rust-psutil/rust-psutil/issues/64 and
+// https://github.com/rust-psutil/rust-psutil/pull/39
+#[allow(clippy::unnecessary_cast)]
 pub fn disk_usage<P>(path: P) -> Result<DiskUsage>
 where
 	P: AsRef<Path>,
 {
 	let statvfs = sys::statvfs::statvfs(path.as_ref())?;
 
-	// need to do the u64 casts since on some platforms the types are aliased to u32
-	// https://github.com/rust-psutil/rust-psutil/issues/64
-	// https://github.com/rust-psutil/rust-psutil/pull/39
+	let total = statvfs.blocks() as u64 * statvfs.fragment_size() as u64;
 
-	let total = statvfs.blocks() * statvfs.fragment_size();
-
-	let avail_to_root = statvfs.blocks_free() * statvfs.fragment_size();
+	let avail_to_root = statvfs.blocks_free() as u64 * statvfs.fragment_size() as u64;
 	let used = total - avail_to_root;
 
-	let free = statvfs.blocks_available() * statvfs.fragment_size();
+	let free = statvfs.blocks_available() as u64 * statvfs.fragment_size() as u64;
 
 	let total_user = used + free;
 	let percent = ((used as f64 / total_user as f64) * 100.0) as f32;
